b6d0a27cf97df7676b505840f110ff169b20e5b8
Kawaikikinou <tonynogo@gmail.com>
2024-03-07 22:54:46 +0100

    Collision scene

---
+++ MHServerEmuUnityTools/Assets/Scripts/RootController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RootController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+    }
+}
